f3944ef2e2bb7bb4bda634d1842146d20ac0c726
ilya0 <ilya02il@yandex.ru>
2026-06-20 22:37:32 +0700

    LAB 1: Compose Demo - Add DemoScreen Composable

--- ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,4 +72,35 @@ fun DemoSlider(
         modifier = Modifier.padding(10.dp),
         onValueChange = { onPositionChange(it) }
     )
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    var sliderPos by remember {
+        mutableFloatStateOf(20f)
+    };
+
+    val handlePosChange = {
+        pos: Float -> sliderPos = pos
+    };
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DemoText(
+            fontSize = sliderPos,
+            message = "Welcome to Compose"
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+        DemoSlider(
+            sliderPosition = sliderPos,
+            onPositionChange = handlePosChange
+        )
+        Text(
+            text = sliderPos.toInt().toString() + "sp",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
