254ca544cf782df6082b91f62410c9c26cb86d56
ilya0 <ilya02il@yandex.ru>
2026-06-20 23:07:17 +0700

    LAB 2: Modifiers Demo - Reorder Modifiers

--- ModifiersDemo/app/src/main/java/com/example/modifiersdemo/MainActivity.kt
+++ ModifiersDemo/app/src/main/java/com/example/modifiersdemo/MainActivity.kt
@@ -33,8 +33,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
     val customized = modifier
-        .border(width = 2.dp, color = Color.Black)
         .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Black)
 
     Text(
         "Hello Compose",
