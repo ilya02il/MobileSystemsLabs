959d547d46bc9416588468134bb3d8fe1ebb56a8
ilya0 <ilya02il@yandex.ru>
2026-06-20 22:31:47 +0700

    LAB 1: Compose Demo - Add DemoSlider Composable

--- ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
@@ -44,4 +48,17 @@ fun DemoTextPreview() {
             message = "Welcome to Android"
         )
     }
+}
+
+@Composable
+fun DemoSlider(
+    sliderPosition: Float,
+    onPositionChange: (Float) -> Unit
+) {
+    Slider(
+        value = sliderPosition,
+        valueRange = 20f..38f,
+        modifier = Modifier.padding(10.dp),
+        onValueChange = { onPositionChange(it) }
+    )
 }
