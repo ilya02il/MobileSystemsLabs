f840d9540399184e4b9171d46bd84bc15ff08058
ilya0 <ilya02il@yandex.ru>
2026-06-20 22:28:49 +0700

    LAB 1: Compose Demo - Add DemoText Composable

--- ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,32 +17,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Compose",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DemoText(
+                    fontSize = 12f,
+                    message = "Welcome to Android"
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DemoText(message: String, fontSize: Float) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
     )
 }
 
 @Composable
-@Preview(
-    showSystemUi = true,
-    showBackground = true
-)
-fun GreetingPreview() {
+@Preview(showBackground = true)
+fun DemoTextPreview() {
     ComposeDemoTheme {
-        Greeting("Android")
+        DemoText(
+            fontSize = 12f,
+            message = "Welcome to Android"
+        )
     }
 }
