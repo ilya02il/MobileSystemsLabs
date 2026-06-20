5bdcd6d9c6931a24df8e6251891fecbc21cf2e29
ilya0 <ilya02il@yandex.ru>
2026-06-20 23:14:23 +0700

    LAB 2: Modifiers Demo - Change DemoScreen

--- ModifiersDemo/app/src/main/java/com/example/modifiersdemo/MainActivity.kt
+++ ModifiersDemo/app/src/main/java/com/example/modifiersdemo/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -38,16 +43,24 @@ fun DemoScreen(modifier: Modifier = Modifier) {
         .padding(all = 10.dp)
         .border(width = 2.dp, color = Color.Black)
 
-    Text(
-        "Hello Compose",
-        fontSize = 40.sp,
-        modifier = customized,
-        fontWeight = FontWeight.Bold
-    )
+    Column(
+        Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Hello Compose",
+            fontSize = 40.sp,
+            modifier = customized,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(Modifier.height(16.dp))
+        CustomImage(R.drawable.vacation)
+    }
 }
 
 @Composable
-fun CustomImage(image: Int, modifier: Modifier) {
+fun CustomImage(image: Int, modifier: Modifier = Modifier) {
     Image(
         modifier = modifier,
         contentDescription = null,
