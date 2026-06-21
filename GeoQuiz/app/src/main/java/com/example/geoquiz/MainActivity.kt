5069c53e0582b71ae8ff3b53b694db3c128600de
ilya0 <ilya02il@yandex.ru>
2026-06-21 12:20:31 +0700

    LAB 4: GeoQuiz - Add Question data class and questions list

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -16,6 +16,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.geoquiz.ui.theme.GeoQuizTheme
 
+data class Question(val text: String, val answer: Boolean)
+
+val questions = listOf(
+    Question("Canberra is the capital of Australia.", true),
+    Question("The Pacific Ocean is larger than the Atlantic Ocean.", true),
+    Question("The Suez Canal connects the Red Sea and the Indian Ocean.", false),
+    Question("The source of the Nile River is in Egypt.", false),
+    Question("The Amazon River is the longest river in the Americas.", true),
+    Question("Lake Baikal is the world's oldest and deepest freshwater lake.", true)
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
