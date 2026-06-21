b72527fb2b6cec01a4b05a56ac7309a1f5cb7266
ilya0 <ilya02il@yandex.ru>
2026-06-21 11:16:59 +0700

    LAB 3: Tips Calculator - Add TipsCalculatorScreen composable

--- TipsCalculator/app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ TipsCalculator/app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tipscalculator.ui.theme.TipsCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TipsCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TipsCalculatorScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TipsCalculatorScreen(modifier: Modifier = Modifier) {
+    var billAmount by remember { mutableStateOf("") }
+    var dishCount by remember { mutableStateOf("") }
+    var tipPercent by remember { mutableFloatStateOf(0f) }
+
+    val discountPercent = when (dishCount.toIntOrNull() ?: 0) {
+        in 1..2 -> 3
+        in 3..5 -> 5
+        in 6..10 -> 7
+        else -> if ((dishCount.toIntOrNull() ?: 0) > 10) 10 else 0
+    }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TipsCalculatorScreenPreview() {
     TipsCalculatorTheme {
-        Greeting("Android")
+        TipsCalculatorScreen()
     }
 }
