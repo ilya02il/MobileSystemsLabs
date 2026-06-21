6a569b17ddb7f8ef6438bd1d459aa91fe556a4f0
ilya0 <ilya02il@yandex.ru>
2026-06-21 10:47:14 +0700

    LAB 2: Row Cols Demo - Row Scope Element Weight

--- RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -46,21 +46,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
-        Text(
-            text = "Large Text\n\nMore Text",
-            Modifier.alignBy(FirstBaseline),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Small Text",
-            Modifier.paddingFrom(
-                before = 80.dp, after = 0.dp,
-                alignmentLine = FirstBaseline
-            ),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        TextCell("1", Modifier.weight(0.2f, fill = true))
+        TextCell("2", Modifier.weight(0.4f, fill = true))
+        TextCell("3", Modifier.weight(0.3f, fill = true))
     }
 }
 
