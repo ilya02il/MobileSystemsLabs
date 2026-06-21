445e80c3cbcdba64cf52fa5c44761f5e0a5c3a93
ilya0 <ilya02il@yandex.ru>
2026-06-21 10:37:10 +0700

    LAB 2: Row Cols Demo - Arrangement: Column Bottom

--- RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -41,9 +42,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier.width(250.dp),
-        horizontalArrangement = Arrangement.End
+    Column(
+        modifier = modifier.height(400.dp),
+        verticalArrangement = Arrangement.Bottom
     ) {
         TextCell("1")
         TextCell("2")
