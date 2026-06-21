7060b44b12457ec05e846ae4d943305f2fd26657
ilya0 <ilya02il@yandex.ru>
2026-06-21 10:45:39 +0700

    LAB 2: Row Cols Demo - First Baseline Alignment

--- RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,13 +48,16 @@ fun MainScreen(modifier: Modifier = Modifier) {
     Row {
         Text(
             text = "Large Text\n\nMore Text",
-            Modifier.alignBy(LastBaseline),
+            Modifier.alignBy(FirstBaseline),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
             text = "Small Text",
-            Modifier.alignByBaseline(),
+            Modifier.paddingFrom(
+                before = 80.dp, after = 0.dp,
+                alignmentLine = FirstBaseline
+            ),
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold
         )
