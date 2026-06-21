288c9843a568caca8cff720aba53a1f27df891a2
ilya0 <ilya02il@yandex.ru>
2026-06-21 10:41:07 +0700

    LAB 2: Row Cols Demo - Row Scope Alignment

--- RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ RowColDemo/app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -42,13 +42,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier.width(1000.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        TextCell("1")
-        TextCell("2")
-        TextCell("3")
+    Row(modifier = modifier.height(300.dp)) {
+        TextCell("1", Modifier.align(Alignment.Top))
+        TextCell("2", Modifier.align(Alignment.CenterVertically))
+        TextCell("3", Modifier.align(Alignment.Bottom))
     }
 }
 
