99914ddd698bba5218fe267841c4c85484440ed0
ilya0 <ilya02il@yandex.ru>
2026-06-24 22:20:44 +0700

    LAB 8: Gesture Demo - Add ScrollableModifier with scrollable modifier

--- GestureDemo/app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ GestureDemo/app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -52,7 +54,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    PointerInputDrag(modifier)
+    ScrollableModifier(modifier)
 }
 
 @Composable
@@ -75,6 +77,30 @@ fun ClickDemo(modifier: Modifier = Modifier) {
             .background(bgColor)
             .size(100.dp)
     )
+}
+
+@Composable
+fun ScrollableModifier(modifier: Modifier = Modifier) {
+    var offset by remember { mutableStateOf(0f) }
+
+    Box(
+        modifier
+            .fillMaxSize()
+            .scrollable(
+                orientation = Orientation.Vertical,
+                state = rememberScrollableState { distance ->
+                    offset += distance
+                    distance
+                }
+            )
+    ) {
+        Box(
+            modifier = Modifier
+                .size(90.dp)
+                .offset { IntOffset(0, offset.roundToInt()) }
+                .background(Color.Red)
+        )
+    }
 }
 
 @Composable
