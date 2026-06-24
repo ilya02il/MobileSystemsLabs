591907b906bb62da19a88fae49dd4b0372f22cec
ilya0 <ilya02il@yandex.ru>
2026-06-25 00:02:59 +0700

    LAB 10: Photo Gallery - Display photos in LazyVerticalGrid using Coil

--- PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,10 +35,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotoGalleryScreen() {
-    Box(
+fun PhotoGalleryScreen(viewModel: PhotoGalleryViewModel = viewModel()) {
+    val items by viewModel.galleryItems.collectAsState()
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Gray)
-    )
+    ) {
+        items(items) { item ->
+            AsyncImage(
+                model = item.thumbnailUrl,
+                contentDescription = item.author,
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .padding(2.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
 }
