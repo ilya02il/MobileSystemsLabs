379f4be094d2deb8a5f747bd396c05fb5cb04af5
ilya0 <ilya02il@yandex.ru>
2026-06-24 21:50:48 +0700

    LAB 7: Glance Widget - Add StockAppWidget and StockAppWidgetReceiver

---
+++ GlanceWidget/app/src/main/java/com/example/glancewidget/glance/StockAppWidget.kt
@@ -0,0 +1,155 @@
+package com.example.glancewidget.glance
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.action.clickable
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.updateAll
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import com.example.glancewidget.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.time.Duration
+import java.util.Locale
+
+class StockAppWidget : GlanceAppWidget() {
+
+    private var job: Job? = null
+
+    companion object {
+        private val smallMode = DpSize(100.dp, 80.dp)
+        private val mediumMode = DpSize(120.dp, 120.dp)
+    }
+
+    override val sizeMode: SizeMode = SizeMode.Responsive(
+        setOf(smallMode, mediumMode)
+    )
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        if (job == null) {
+            job = startUpdateJob(
+                Duration.ofSeconds(20).toMillis(),
+                context
+            )
+        }
+
+        provideContent {
+            GlanceTheme {
+                GlanceContent()
+            }
+        }
+    }
+
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+                StockAppWidget().updateAll(context)
+                delay(timeInterval)
+            }
+        }
+    }
+
+    private fun refreshPrice() {
+        PriceDataRepo.update()
+    }
+
+    @Composable
+    fun GlanceContent() {
+        val stateCount by PriceDataRepo.currentPrice.collectAsState()
+        val size = androidx.glance.LocalSize.current
+        when (size) {
+            smallMode -> Small(stateCount)
+            mediumMode -> Medium(stateCount)
+        }
+    }
+
+    @Composable
+    private fun StockDisplay(stateCount: Float) {
+        val color = if (PriceDataRepo.change > 0) {
+            GlanceTheme.colors.primary
+        } else {
+            GlanceTheme.colors.error
+        }
+        val textStyle = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = color
+        )
+
+        Text(
+            PriceDataRepo.ticker, style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Text(
+            text = String.format(Locale.getDefault(), "%.2f", stateCount),
+            style = textStyle
+        )
+        Text("${PriceDataRepo.change} %", style = textStyle)
+    }
+
+    @Composable
+    private fun Small(stateCount: Float) {
+        Column(
+            modifier = GlanceModifier
+                .clickable { refreshPrice() }
+                .fillMaxSize()
+                .background(GlanceTheme.colors.background)
+                .padding(8.dp)
+        ) {
+            StockDisplay(stateCount)
+        }
+    }
+
+    @Composable
+    private fun Medium(stateCount: Float) {
+        Column(
+            horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
+            modifier = GlanceModifier
+                .clickable { refreshPrice() }
+                .fillMaxSize()
+                .cornerRadius(15.dp)
+                .background(GlanceTheme.colors.background)
+                .padding(8.dp)
+        ) {
+            StockDisplay(stateCount)
+            Image(
+                provider = ImageProvider(
+                    if (PriceDataRepo.change > 0) R.drawable.up_arrow
+                    else R.drawable.down_arrow
+                ),
+                contentDescription = "Arrow Image",
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+            )
+        }
+    }
+}
