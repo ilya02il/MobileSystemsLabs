cb2c8c4c0a6924c6229ca1c202a33b4d6fc2fbd7
ilya0 <ilya02il@yandex.ru>
2026-06-24 23:40:04 +0700

    LAB 10: Photo Gallery - Add PhotoGalleryScreen composable

--- PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PhotoGalleryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PhotoGalleryScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PhotoGalleryScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhotoGalleryTheme {
-        Greeting("Android")
-    }
 }
